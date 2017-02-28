285e1fa714c0a206f4a1f6a20d42431b3f73f67a
rollingscopes2017 <vitas97@tut.by>
2017-03-01 00:10:06 +0300

    js-assigments/feature: solve date tasks

--- task/03-date-tasks.js
+++ task/03-date-tasks.js
@@ -22,7 +22,7 @@
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
 function parseDataFromRfc2822(value) {
-   throw new Error('Not implemented');
+   return new Date(value);
 }
 
 /**
@@ -37,7 +37,7 @@ function parseDataFromRfc2822(value) {
  *    '2016-01-19T08:07:37Z' => Date()
  */
 function parseDataFromIso8601(value) {
-   throw new Error('Not implemented');
+   return new Date(value);
 }
 
 
@@ -56,7 +56,9 @@ function parseDataFromIso8601(value) {
  *    Date(2015,1,1)    => false
  */
 function isLeapYear(date) {
-   throw new Error('Not implemented');
+  return date.getFullYear() % 4 === 0
+          && (date.getFullYear() % 100 !== 0
+          || date.getFullYear() % 400 === 0);
 }
 
 
@@ -76,14 +78,22 @@ function isLeapYear(date) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
 function timeSpanToString(startDate, endDate) {
-   throw new Error('Not implemented');
+  const pad = (number) => {
+    let str = '' + number;
+    while (str.length < 3) {
+        str = '0' + str;
+    }
+    return str;
+  };
+  let date = new Date(endDate - startDate);
+  return `${date.toLocaleTimeString('en-US', { timeZone: 'UTC', hour12: false })}.${pad(date.getMilliseconds() % 1000)}`;
 }
 
 
 /**
  * Returns the angle (in radians) between the hands of an analog clock for the specified Greenwich time.
  * If you have problem with solution please read: https://en.wikipedia.org/wiki/Clock_angle_problem
- * 
+ *
  * @param {date} date
  * @return {number}
  *
@@ -94,7 +104,10 @@ function timeSpanToString(startDate, endDate) {
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
 function angleBetweenClockHands(date) {
-    throw new Error('Not implemented');
+  let hour = date.getUTCHours() > 13 ? date.getUTCHours() - 12 : date.getUTCHours();
+  let minutes = date.getMinutes();
+  let degree = Math.abs(((hour * 30) + (minutes * 0.5)) - (minutes * 6));
+  return Math.PI * Math.min(degree, 360 - degree) / 180;
 }
 
 
